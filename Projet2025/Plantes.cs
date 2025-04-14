95b57f91ca8a90dcf22de935c7f7879c10debf9e
lamaire <leavamaire@gmail.com>
2025-04-14 12:24:18 +0200

    Ajout Plantes

--- Projet2025/Plantes.cs
+++ Projet2025/Plantes.cs
@@ -1,7 +1,20 @@
-public abstract class Plantes {
+public abstract class Plantes 
+{
 
-    protected string Nom {get;}
-    protected List<string> SaisonsSemis {get; set;}
+    public string Nom {get;}
+    public List<string> ListeSaisons {get; set;}
+    public string TerrainPref {get;}
+    public int Espacement {get;}
+    public int Place {get;}
+    public double VitesseCroissance {get;}
+    public int BesoinEau {get;}
+    public int BesoinLum {get;}
+    public double TempMax {get;}
+    public double TempMin {get;}
+    public List<string> ListeMaladies {get;} // a changer, List<Maladies>
+    public int EsperenceVie {get;} // en mois
+    public int NbFruitsMax {get;} // nb de fruits produits par le semi au maximum
+
     
 
     
