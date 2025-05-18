97c1ef5634b09bc7730350826c00e4b382130d9a
lamaire <leavamaire@gmail.com>
2025-05-18 21:44:03 +0200

    ajouts

--- Projet2025/obj/Debug/net9.0/Projet2025.AssemblyInfo.cs
+++ Projet2025/obj/Debug/net9.0/Projet2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38226eb8ae105235d9fbedb2b403734c9c4371ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182f90a8d5f334267fb5acef63ee3536dfbdbc36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
