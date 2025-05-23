7fe839d6d680cbf93205da49121a07ffd62aa26e
lamaire <leavamaire@gmail.com>
2025-05-23 15:45:24 +0200

    merge léa

--- Projet2025/obj/Debug/net9.0/Projet2025.AssemblyInfo.cs
+++ Projet2025/obj/Debug/net9.0/Projet2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c688123ebad6eafefaf5441bccee6b421a2f33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ebbc8f7ea8eb6855b4eef63b9227f8a9570269")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
